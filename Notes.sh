3cbe7d1649564ce7835a8f531da9f6fed190cd04
hubbylei <hubbylei@hubbylei.dynu.net>
2025-11-26 22:22:38 +0800

    Update Notes.sh

--- Notes.sh
+++ Notes.sh
@@ -5,9 +5,9 @@ cd ${workdir}/Magisk
 release_tag=$(git rev-parse HEAD | cut -c 1-8)
 ver=$(grep "magisk.versionCode" app/gradle.properties | awk -F "=" '{print $2}')
 echo "## Magisk (${release_tag}) (${ver})" > ${workdir}/out/notes.md
-json=$(curl -sL -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' https://api.github.com/repos/topjohnwu/Magisk/commits | jq .)
-jsha=$(echo $json | jq .[].sha | sed 's/\"//g')
-hsha=$(curl -sL -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' https://api.github.com/repos/hubbylei/Magisk-Files/commits | jq .[0].commit.message | sed 's/\"//g' | awk -F "-" '{print $1}')
+json=$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/commits)
+jsha=$(echo $json | jq -r .[].sha)
+hsha=$(curl -sL https://api.github.com/repos/hubbylei/Magisk-Files/commits | jq -r .[0].commit.message | awk -F "-" '{print $1}')
 sha=()
 i=0
 for s in $jsha
@@ -20,5 +20,7 @@ do
   if [ ${sha[$i]} == $hsha ];then
       break
   fi
-  echo "- "$(echo $json | jq .[$i].commit.message | sed 's/\\n/<br>/g' | sed 's/\"//g') >> ${workdir}/out/notes.md
+  message=$(echo $json | jq .[$i].commit.message)
+  echo ${message}
+  echo "- ${message}" | sed 's/\"//g' | sed 's/\\n/<br>/g' >> ${workdir}/out/notes.md
 done
